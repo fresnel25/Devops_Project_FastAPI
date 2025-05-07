7ef8b255dd8a5a7822acfa4a0b41bcc93d959fd5
fresnel25 <ngaleufresnel@gmail.com>
2025-05-07 16:46:47 +0200

    change database use postgresql

--- config/db.py
+++ config/db.py
@@ -5,15 +5,15 @@ import os
 # Charger les variables d'environnement depuis le fichier .env
 load_dotenv()
 
-# Lire les variables
+# Lire les variables PostgreSQL
 DB_HOST = os.getenv("DB_HOST")
 DB_PORT = os.getenv("DB_PORT")
 DB_NAME = os.getenv("DB_NAME")
 DB_USER = os.getenv("DB_USER")
 DB_PASSWORD = os.getenv("DB_PASSWORD")
 
-# Construire l'URL de connexion
-DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+# Construire l'URL de connexion PostgreSQL
+DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
 # Connexion SQLAlchemy
 engine = create_engine(DATABASE_URL)
