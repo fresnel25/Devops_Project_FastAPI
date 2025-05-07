7333868790a362987b170e8cb2c2efa01368e7c7
fresnel25 <ngaleufresnel@gmail.com>
2025-05-07 15:22:30 +0200

    fix pipeline

--- config/db.py
+++ config/db.py
@@ -1,5 +1,21 @@
 from sqlalchemy import create_engine, MetaData
+from dotenv import load_dotenv
+import os
 
-engine = create_engine("mysql+pymysql://root@localhost:3306/devop_crud_db")
+# Charger les variables d'environnement depuis le fichier .env
+load_dotenv()
+
+# Lire les variables
+DB_HOST = os.getenv("DB_HOST")
+DB_PORT = os.getenv("DB_PORT")
+DB_NAME = os.getenv("DB_NAME")
+DB_USER = os.getenv("DB_USER")
+DB_PASSWORD = os.getenv("DB_PASSWORD")
+
+# Construire l'URL de connexion
+DATABASE_URL = f"mysql+pymysql://{DB_USER}:{DB_PASSWORD}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
+
+# Connexion SQLAlchemy
+engine = create_engine(DATABASE_URL)
 meta = MetaData()
 con = engine.connect()
